64b00adfa2a6893e5220b7a729be23ff5546d519
Arslan Ahmad <ars.ahm6@gmail.com>
2020-01-13 17:33:21 +0500

    Add service for restaurant data.

--- Plural Sight/Fundamentals/MainSolution/OdeToFoodWebApp/Pages/Restaurants/List.cshtml.cs
+++ Plural Sight/Fundamentals/MainSolution/OdeToFoodWebApp/Pages/Restaurants/List.cshtml.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFoodData;
 
 namespace OdeToFoodWebApp.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
         public void OnGet()
         {
         }
