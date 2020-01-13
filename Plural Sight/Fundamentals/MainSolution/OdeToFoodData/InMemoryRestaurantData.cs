476b03aa186556aee4bbddc79a4bd6f52d51d146
Arslan Ahmad <ars.ahm6@gmail.com>
2020-01-13 18:55:09 +0500

    Added details button in the list.

--- Plural Sight/Fundamentals/MainSolution/OdeToFoodData/InMemoryRestaurantData.cs
+++ Plural Sight/Fundamentals/MainSolution/OdeToFoodData/InMemoryRestaurantData.cs
@@ -14,8 +14,8 @@ namespace OdeToFoodData
             restaurants = new List<Restaurant>
             {
                 new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "MarryLand", Cuisine = CuisineType.Italian},
-                new Restaurant { Id = 1, Name = "Cinnamon Club", Location = "New York", Cuisine = CuisineType.Mexican},
-                new Restaurant { Id = 1, Name = "Bombay Biryani", Location = "London", Cuisine = CuisineType.Indian}
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "New York", Cuisine = CuisineType.Mexican},
+                new Restaurant { Id = 3, Name = "Bombay Biryani", Location = "London", Cuisine = CuisineType.Indian}
             };
 
         }
