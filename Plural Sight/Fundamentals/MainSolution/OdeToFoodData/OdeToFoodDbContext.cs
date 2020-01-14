4ff77f17bcc62557ed2292b5bd7127c4bdb259ae
Arslan Ahmad <ars.ahm6@gmail.com>
2020-01-14 16:02:52 +0500

    Created and initialized a db context.

---
+++ Plural Sight/Fundamentals/MainSolution/OdeToFoodData/OdeToFoodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFoodCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFoodData
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
