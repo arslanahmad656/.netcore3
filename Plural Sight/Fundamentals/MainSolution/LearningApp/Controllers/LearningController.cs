776108f91947f6b00c6c620339c2e76866525857
Asim Kabir <asim_kabir456@outlook.com>
2020-02-01 12:32:50 +0500

    hhhh

---
+++ Plural Sight/Fundamentals/MainSolution/LearningApp/Controllers/LearningController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningApp.Controllers
+{
+    public class LearningController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hi there!");
+        }
+
+        public IActionResult OptOutOfTagHelperTest()
+        {
+            return View(new Movie
+            {
+                ID = 4578,
+                Title = "SAW VII",
+                //ReleaseDate = new DateTime(2007, 12, 11),
+                Genre = "Thriller",
+                Price = 455.98M
+            });
+        }
+    }
+}
