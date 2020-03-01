c7b1bb5c930ebdfc7f470a39cab596f5938f04b1
Arslan Ahmad 656 <arslan_mgl6445@live.com>
2020-03-01 13:01:36 +0500

    Modified startup file to enable MVC.

--- Plural Sight/Fundamentals/MainSolution/TicTacToe/Startup.cs
+++ Plural Sight/Fundamentals/MainSolution/TicTacToe/Startup.cs
@@ -16,6 +16,7 @@ namespace TicTacToe
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,16 @@ namespace TicTacToe
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            else
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(e => e.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"));
         }
     }
 }
